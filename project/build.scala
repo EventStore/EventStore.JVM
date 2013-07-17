4a34e8eccc756542dbbf02df592df02d3083d915
Yaroslav Klymko <t3hnar@gmail.com>
2013-07-17 20:23:31 +0300

    cleanup

--- project/build.scala
+++ project/build.scala
@@ -3,23 +3,11 @@ import Keys._
 import scalabuff.ScalaBuffPlugin._
 
 object build extends Build {
-
-  val depsRepos = Seq(
-    "TnmPublic"  at "http://nexus.thenewmotion.com/content/groups/public",
-    "Releases"   at "http://nexus.thenewmotion.com/content/repositories/releases",
-    "Snapshots"  at "http://nexus.thenewmotion.com/content/repositories/snapshots",
-    "spray repo" at "http://repo.spray.io"
-  )
-
   lazy val basicSettings = Seq(
     name := "eventstore-client",
-    organization    := "com.geteventstore",
+    organization    := "com.eventstore",
     scalaVersion    := "2.10.2",
-    resolvers       ++= depsRepos,
-    scalacOptions   := Seq(
-      "-encoding", "UTF-8",
-      "-unchecked",
-      "-deprecation"),
+    scalacOptions   := Seq("-encoding", "UTF-8", "-unchecked", "-deprecation"),
     parallelExecution in Compile := true
   )
 
