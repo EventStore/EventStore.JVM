fdff6e3c223a9b01479fd8b433b0ee34c460d5d3
Yaroslav Klymko <t3hnar@gmail.com>
2013-07-23 00:35:18 +0300

    add FixedLengthFieldFrame as workaround for https://www.assembla.com/spaces/akka/tickets/3520

---
+++ src/main/scala/eventstore/tcp/FixedLengthFieldFrame.scala
@@ -0,0 +1,39 @@
+package eventstore.tcp
+
+import java.nio.ByteOrder
+import akka.io.{SymmetricPipePair, PipelineContext, LengthFieldFrame}
+import akka.util.ByteString
+
+/**
+ * @author Yaroslav Klymko
+ */
+class FixedLengthFieldFrame(maxSize: Int,
+                            byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN,
+                            lengthIncludesHeader: Boolean = true)
+  extends LengthFieldFrame(maxSize, byteOrder, 4, lengthIncludesHeader) {
+
+  override def apply(ctx: PipelineContext) = {
+
+    val superPipePair = super.apply(ctx)
+
+    new SymmetricPipePair[ByteString, ByteString] {
+      implicit val byteOrder = FixedLengthFieldFrame.this.byteOrder
+
+      def eventPipeline = superPipePair.eventPipeline
+
+
+      override def commandPipeline = {
+        bs: ByteString =>
+          val length =
+            if (lengthIncludesHeader) bs.length + 4 else bs.length
+          if (length > maxSize) Seq()
+          else {
+            val bb = ByteString.newBuilder
+            bb.putInt(length)
+            bb ++= bs
+            ctx.singleCommand(bb.result())
+          }
+      }
+    }
+  }
+}
