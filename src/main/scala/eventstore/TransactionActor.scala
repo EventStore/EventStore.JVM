9664ad55602c39b1be8fddee741cddedff77fad6
Yaroslav Klymko <t3hnar@gmail.com>
2013-12-19 00:26:23 +0200

    improve TransactionActor

--- src/main/scala/eventstore/TransactionActor.scala
+++ src/main/scala/eventstore/TransactionActor.scala
@@ -1,49 +1,103 @@
 package eventstore
 
-import akka.actor.{ Props, ActorRef, Actor }
+import akka.actor.{ ActorLogging, Props, ActorRef, Actor }
+import scala.collection.immutable.Queue
+import akka.actor.Status.Failure
 
 /**
  * @author Yaroslav Klymko
  */
+object TransactionActor {
 
-object Transaction {
-  case class Write(events: Seq[EventData])
+  sealed trait Kickoff
+  case class Start(data: TransactionStart) extends Kickoff
+  case class Continue(transactionId: Long) extends Kickoff
+
+  sealed trait Command
+
+  case object GetTransactionId
+  case class TransactionId(value: Long)
+
+  case class Write(events: Seq[EventData]) extends Command
   case object WriteCompleted
 
-  case object Commit
-  case object CommitCompleted
-}
+  case object Commit extends Command
 
-object TransactionActor {
-  def props(connection: ActorRef, client: ActorRef, transactionId: Long, requireMaster: Boolean = true): Props =
-    Props(classOf[TransactionActor], connection, client, transactionId, requireMaster)
+  def props(connection: ActorRef, kickoff: Kickoff, requireMaster: Boolean = true): Props =
+    Props(classOf[TransactionActor], connection, kickoff, requireMaster)
 }
 
 class TransactionActor(
     connection: ActorRef,
-    client: ActorRef,
-    transactionId: Long,
-    requireMaster: Boolean) extends Actor {
-
-  import Transaction._
+    kickoff: TransactionActor.Kickoff,
+    requireMaster: Boolean) extends Actor with ActorLogging {
+  import TransactionActor._
 
   context watch connection
-  context watch client
 
-  val receiveWriteCompleted: Receive = {
-    case TransactionWriteCompleted(`transactionId`) => client ! WriteCompleted
+  def receive: Receive = kickoff match {
+    case Continue(transactionId) => new ContinueReceive(transactionId).apply(Queue())
+    case Start(transactionStart) =>
+      connection ! transactionStart
+      starting(Queue(), Queue())
   }
 
-  val receiveCommitCompleted: Receive = {
-    case TransactionCommitCompleted(`transactionId`) =>
-      client ! CommitCompleted
-      context stop self
+  def starting(stash: Queue[StashEntry], awaitingId: Queue[ActorRef]): Receive = {
+    case x: Command       => context become starting(stash enqueue StashEntry(x), awaitingId)
+    case GetTransactionId => context become starting(stash, awaitingId enqueue sender)
+    case TransactionStartCompleted(transactionId) =>
+      awaitingId.foreach(_ ! TransactionId(transactionId))
+      context become new ContinueReceive(transactionId).apply(stash)
+    case failure @ Failure(error) =>
+      awaitingId.foreach(_ ! failure)
+      throw error
   }
 
-  def receive = receiveWriteCompleted orElse {
-    case Write(events) => connection ! TransactionWrite(transactionId, events, requireMaster)
-    case Commit =>
+  class ContinueReceive(transactionId: Long) extends (Queue[StashEntry] => Receive) {
+    val common: Receive = {
+      case GetTransactionId => sender ! TransactionId(transactionId)
+    }
+
+    val empty: Receive = common orElse {
+      case Commit        => context become commit(sender)
+      case Write(events) => context become write(events, sender, Queue())
+    }
+
+    def failure(client: ActorRef): Receive = {
+      case failure @ Failure(error) =>
+        client ! failure
+        throw error
+    }
+
+    def commit(client: ActorRef): Receive = {
       connection ! TransactionCommit(transactionId, requireMaster)
-      context become (receiveWriteCompleted orElse receiveCommitCompleted)
+      common orElse failure(client) orElse {
+        case TransactionCommitCompleted(`transactionId`) => context stop self
+      }
+    }
+
+    def write(events: Seq[EventData], client: ActorRef, stash: Queue[StashEntry]): Receive = {
+      connection ! TransactionWrite(transactionId, events, requireMaster)
+      writing(client, stash)
+    }
+
+    def writing(client: ActorRef, stash: Queue[StashEntry]): Receive = common orElse failure(client) orElse {
+      case x: Command => context become writing(client, stash enqueue StashEntry(x))
+      case TransactionWriteCompleted(`transactionId`) =>
+        client ! WriteCompleted
+        context become apply(stash)
+    }
+
+    def apply(stash: Queue[StashEntry]): Receive =
+      if (stash.isEmpty) empty
+      else {
+        val (StashEntry(command, client), tail) = stash.dequeue
+        command match {
+          case Commit        => commit(client)
+          case Write(events) => write(events, client, tail)
+        }
+      }
   }
+
+  case class StashEntry(command: Command, client: ActorRef = sender)
 }
--- src/test/scala/eventstore/TransactionActorSpec.scala
+++ src/test/scala/eventstore/TransactionActorSpec.scala
@@ -4,7 +4,8 @@ import akka.actor.Terminated
 import akka.testkit.{ TestProbe, TestActorRef }
 import scala.concurrent.duration._
 import util.ActorSpec
-import Transaction._
+import TransactionActor._
+import akka.actor.Status.Failure
 
 /**
  * @author Yaroslav Klymko
@@ -12,40 +13,190 @@ import Transaction._
 class TransactionActorSpec extends ActorSpec {
   "TransactionActor" should {
 
-    "write" in new TestScope {
-      val events = Seq(EventData(eventType = "test"))
-      actor ! Write(events)
-      expectMsg(TransactionWrite(transactionId, events))
+    "start" in new StartTransactionScope {
+      actor ! GetTransactionId
+      actor ! Write(es1)
+      actor ! Write(es2)
 
-      actor ! TransactionWriteCompleted(invalid)
-      client.expectNoMsg(300.millis)
+      connection.expectMsg(kickOff.data)
+      expectNoMsgs()
 
-      actor ! TransactionWriteCompleted(transactionId)
-      client.expectMsg(WriteCompleted)
+      startCompleted
+
+      expectMsg(TransactionId(transactionId))
+
+      expectWrite(es1)
+
+      expectNoMsgs()
+
+      actor ! Write(es3)
+      writeCompleted()
+
+      expectWrite(es2)
+      writeCompleted()
+
+      expectMsg(WriteCompleted)
+      expectMsg(WriteCompleted)
+
+      expectWrite(es3)
+      writeCompleted()
+      expectMsg(WriteCompleted)
+
+      verifyTransactionId()
     }
 
-    "commit" in new TestScope {
+    "write" in new ContinueTransactionScope {
+      actor ! Write(es1)
+      actor ! Write(es2)
+      expectWrite(es1)
+
+      writeCompleted(invalid)
+      expectNoMsgs()
+
+      writeCompleted()
+      expectMsg(WriteCompleted)
+
+      expectWrite(es2)
+
+      verifyTransactionId()
+
+      writeCompleted()
+      expectMsg(WriteCompleted)
+    }
+
+    "commit" in new ContinueTransactionScope {
       actor ! Commit
-      expectMsg(TransactionCommit(transactionId))
+      expectCommit
 
-      actor ! TransactionCommitCompleted(invalid)
-      client.expectNoMsg(300.millis)
+      expectNoMsgs()
+      verifyTransactionId()
 
-      actor ! TransactionCommitCompleted(transactionId)
-      client.expectMsg(CommitCompleted)
+      writeCompleted(invalid)
 
-      val probe = TestProbe()
-      probe watch actor
-      probe.expectMsgPF() {
-        case Terminated(`actor`) =>
-      }
+      expectNoMsgs()
+
+      commitCompleted
+
+      expectTerminated
+    }
+
+    "commit from stash" in new ContinueTransactionScope {
+      actor ! Write(es1)
+      actor ! Write(es2)
+      actor ! Commit
+      actor ! Write(es3)
+
+      expectWrite(es1)
+      expectNoMsgs()
+      verifyTransactionId()
+
+      writeCompleted()
+      expectMsg(WriteCompleted)
+
+      expectWrite(es2)
+      expectNoMsgs()
+      verifyTransactionId()
+
+      writeCompleted()
+      expectMsg(WriteCompleted)
+
+      expectCommit
+      expectNoMsgs()
+      verifyTransactionId()
+
+      commitCompleted
+
+      expectTerminated
+    }
+
+    "start and commit" in new StartTransactionScope {
+      actor ! Commit
+      actor ! Write(es1)
+      actor ! Write(es2)
+
+      connection.expectMsg(kickOff.data)
+      startCompleted
+      expectCommit
+      commitCompleted
+      expectTerminated
+      expectNoMsgs()
+    }
+
+    "handle failures while starting" in new StartTransactionScope {
+      sendFailure
+    }
+
+    "handle failures while starting and reply with it on GetTransactionId" in new StartTransactionScope {
+      actor ! GetTransactionId
+      sendFailure
+      expectFailure
+    }
+
+    "handle failures while writing" in new ContinueTransactionScope {
+      actor ! Write(es1)
+      expectWrite(es1)
+      sendFailure
+      expectFailure
+    }
+
+    "handle failures while committing" in new ContinueTransactionScope {
+      actor ! Commit
+      expectCommit
+      sendFailure
+      expectFailure
     }
   }
 
   trait TestScope extends ActorScope {
+    val es1 = events("1")
+    val es2 = events("2")
+    val es3 = events("3")
+
     val transactionId = 0
     val invalid = 1
-    val client = TestProbe()
-    val actor = TestActorRef(TransactionActor.props(testActor, client.ref, transactionId))
+    val connection = TestProbe()
+    val actor = watch(TestActorRef(TransactionActor.props(connection.ref, kickOff)))
+
+    def expectNoMsgs() {
+      val duration = 200.millis
+      expectNoMsg(duration)
+      connection.expectNoMsg(duration)
+    }
+
+    def startCompleted = actor ! TransactionStartCompleted(transactionId)
+
+    def expectWrite(xs: Seq[EventData]) = connection.expectMsg(TransactionWrite(transactionId, xs))
+    def writeCompleted(transactionId: Long = this.transactionId) = actor ! TransactionWriteCompleted(transactionId)
+
+    def expectCommit = connection.expectMsg(TransactionCommit(transactionId))
+    def commitCompleted = actor ! TransactionCommitCompleted(transactionId)
+
+    def expectTerminated = expectMsgPF() {
+      case Terminated(`actor`) =>
+    }
+
+    val failure = Failure(EventStoreException(EventStoreError.AccessDenied))
+
+    def sendFailure {
+      (actor ! failure) mustNotEqual throwAn[EventStoreException]
+    }
+
+    def expectFailure = expectMsg(failure)
+
+    def verifyTransactionId() {
+      actor ! GetTransactionId
+      expectMsg(TransactionId(transactionId))
+    }
+
+    def events(label: String) = Seq(EventData(eventType = label))
+    def kickOff: Kickoff
+  }
+
+  trait StartTransactionScope extends TestScope {
+    def kickOff = Start(TransactionStart(EventStream.Id("stream")))
+  }
+
+  trait ContinueTransactionScope extends TestScope {
+    def kickOff = Continue(transactionId)
   }
 }
