41b99c3654e42f0b2cdc29108897b3adc1e8d97e
Yaroslav Klymko <t3hnar@gmail.com>
2014-09-18 21:37:05 +0200

    separate broken tests in ContentTypeITest

--- src/test/scala/eventstore/ContentTypeITest.scala
+++ src/test/scala/eventstore/ContentTypeITest.scala
@@ -3,30 +3,47 @@ package eventstore
 import ContentType._
 
 class ContentTypeITest extends TestConnection {
+  // TODO report issue to @gregoryyoung
+  val broken = "Does not work in 3.0.0rc9, EventStore always returns Data's ContentType for Metadata"
+
   "content type" should {
-    "be correctly stored and retrieved" in new TestConnectionScope {
-      val contentTypes = /*Unknown(Known.last.value + 1) :: TODO*/ Known.toList
-      val variants = for {
-        dataContentType <- contentTypes
-        metadataContentType <- contentTypes
-      } yield dataContentType -> metadataContentType
+    "be correctly stored and retrieved for Binary/Binary" in new ContentTypeScope {
+      verify(Binary, Binary)
+    }
 
-      def content(x: ContentType): Content = Content(x match {
-        case ContentType.Json   => ByteString(s"""{"data":"data", "contentType":"$x"}""")
-        case ContentType.Binary => ByteString(Array[Byte](0, 1, 2, 3, 4, 5, 6, 7))
-      }, x)
+    "be correctly stored and retrieved for Json/Json" in new ContentTypeScope {
+      verify(Json, Json)
+    }
 
-      foreach(variants) {
-        case (dataContentType, metadataContentType) =>
-          val expected = EventData(
-            eventType = s"$dataContentType/$metadataContentType",
-            data = content(dataContentType),
-            metadata = content(metadataContentType))
+    "be correctly stored and retrieved for Binary/Json" in new ContentTypeScope {
+      verify(Binary, Json)
+    }.pendingUntilFixed(broken)
 
-          val number = append(expected).number
-          val actual = readEventCompleted(number).data
-          actual mustEqual expected
+    "be correctly stored and retrieved for Json/Binary" in new ContentTypeScope {
+      verify(Json, Binary)
+    }.pendingUntilFixed(broken)
+  }
+
+  private trait ContentTypeScope extends TestConnectionScope {
+    def render(x: ContentType) = x.toString.replace("ContentType.", "")
+
+    def content(x: ContentType): Content = {
+      val value = x match {
+        case ContentType.Json => ByteString(s"""{"test":"test"}""")
+        case _                => ByteString(Array[Byte](0, 1, 2))
       }
-    }.pendingUntilFixed
+      Content(value, x)
+    }
+
+    def verify(dataContentType: ContentType, metadataContentType: ContentType) = {
+      val expected = EventData(
+        eventType = s"${render(dataContentType)}/${render(metadataContentType)}",
+        data = content(dataContentType),
+        metadata = content(metadataContentType))
+
+      val number = append(expected).number
+      val actual = readEventCompleted(number).data
+      actual mustEqual expected
+    }
   }
 }
