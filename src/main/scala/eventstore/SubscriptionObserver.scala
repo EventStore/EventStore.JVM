3db96a808a2801156baccb1fae365a959a9a0280
Yaroslav Klymko <t3hnar@gmail.com>
2014-09-22 19:04:51 -0400

    move out SubscriptionObserver to own file

---
+++ src/main/scala/eventstore/SubscriptionObserver.scala
@@ -0,0 +1,33 @@
+package eventstore
+
+import java.io.Closeable
+
+trait SubscriptionObserver[T] {
+  /**
+   * The action liveProcessingStarted is called when the subscription switches from
+   * the reading phase to the live subscription phase.
+   *
+   * @param subscription A [[Closeable]] representing subscription which can be closed.
+   */
+  def onLiveProcessingStart(subscription: Closeable)
+
+  /**
+   * Method invoked when a new event is received over the subscription
+   *
+   * @param event A new event pushed to the subscription
+   * @param subscription A [[Closeable]] representing subscription which can be closed.
+   */
+  def onEvent(event: T, subscription: Closeable)
+
+  /**
+   * Method invoked if the subscription is dropped due to some error
+   *
+   * @param e An error causes subscription termination
+   */
+  def onError(e: Throwable)
+
+  /**
+   * Method invoked if the subscription is dropped
+   */
+  def onClose()
+}
