df6fe4eb9b8b4d5a877b743b52129421bb375a51
Yaroslav Klymko <t3hnar@gmail.com>
2014-09-24 05:05:32 -0400

    add DeleteStreamBuilder

---
+++ src/main/scala/eventstore/j/DeleteStreamBuilder.scala
@@ -0,0 +1,36 @@
+package eventstore
+package j
+
+import Builder.RequireMasterSnippet
+
+class DeleteStreamBuilder(streamId: String) extends Builder[DeleteStream]
+    with RequireMasterSnippet[DeleteStreamBuilder] {
+
+  protected val _streamId = EventStream.Id(streamId)
+  protected var _hard: Boolean = false
+  protected var _expectVersion: ExpectedVersion.Existing = ExpectedVersion.Any
+
+  def requireMaster(x: Boolean): DeleteStreamBuilder = RequireMasterSnippet.requireMaster(x)
+
+  def expectAnyVersion: DeleteStreamBuilder = expectVersion(ExpectedVersion.Any)
+
+  def expectVersion(x: Int): DeleteStreamBuilder = expectVersion(ExpectedVersion.Exact(x))
+
+  def expectVersion(x: ExpectedVersion.Existing): DeleteStreamBuilder = set {
+    _expectVersion = x
+  }
+
+  def hard(x: Boolean): DeleteStreamBuilder = set {
+    _hard = x
+  }
+
+  def softDelete: DeleteStreamBuilder = hard(x = false)
+
+  def hardDelete: DeleteStreamBuilder = hard(x = true)
+
+  def build = DeleteStream(
+    streamId = _streamId,
+    expectedVersion = _expectVersion,
+    hard = _hard,
+    requireMaster = RequireMasterSnippet.value)
+}
