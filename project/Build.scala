155e8f9a2d70cbf5f43547672ebcdc3c0a32e583
Yaroslav Klymko <t3hnar@gmail.com>
2014-11-16 22:23:10 +0200

    update dependencies, plugins, sbt

--- project/Build.scala
+++ project/Build.scala
@@ -7,27 +7,36 @@ object Build extends Build {
   lazy val basicSettings = Seq(
     name                 := "eventstore-client",
     organization         := "com.geteventstore",
-    scalaVersion         := "2.11.2",
-    crossScalaVersions   := Seq("2.10.4", "2.11.2"),
+    scalaVersion         := "2.11.4",
+    crossScalaVersions   := Seq("2.10.4", "2.11.4"),
     licenses             := Seq("BSD 3-Clause" -> url("http://raw.github.com/EventStore/EventStore.JVM/master/LICENSE")),
     homepage             := Some(new URL("http://github.com/EventStore/EventStore.JVM")),
     organizationHomepage := Some(new URL("http://geteventstore.com")),
     description          := "Event Store JVM Client",
     startYear            := Some(2013),
     scalacOptions        := Seq("-encoding", "UTF-8", "-unchecked", "-deprecation", "-feature"),
-    libraryDependencies ++= Seq(Akka.actor, Akka.testkit, protobuf, specs2, mockito, codec, typesafeConfig))
+    libraryDependencies ++= Seq(
+      Akka.actor, Akka.testkit, protobuf, typesafeConfig, codec,
+      Specs2.core, Specs2.mock, mockito)
+  )
 
   object Akka {
     val actor   = apply("actor")
     val testkit = apply("testkit") % "test"
 
-    private def apply(x: String) = "com.typesafe.akka" %% s"akka-$x" % "2.3.6"
+    private def apply(x: String) = "com.typesafe.akka" %% s"akka-$x" % "2.3.7"
+  }
+
+  object Specs2 {
+    val core = apply("core")
+    val mock = apply("mock")
+
+    private def apply(x: String) = "org.specs2" %% s"specs2-$x" % "2.4.11" % "test"
   }
 
   val typesafeConfig = "com.typesafe" % "config" % "1.2.1"
   val codec          = "org.apache.directory.studio" % "org.apache.commons.codec" % "1.8"
   val protobuf       = "com.google.protobuf" % "protobuf-java" % "2.5.0"
-  val specs2         = "org.specs2" %% "specs2" % "2.3.13" % "test"
   val mockito        = "org.mockito" % "mockito-all" % "1.9.5" % "test"
 
   def itFilter(name: String): Boolean = name endsWith "ITest"
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,9 +1,9 @@
-addSbtPlugin("com.github.gseitz" % "sbt-protobuf" % "0.3.2")
+addSbtPlugin("com.github.gseitz" % "sbt-protobuf" % "0.3.3")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.3.0")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8")
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
 
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8")
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.5")
 
 addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.1")
