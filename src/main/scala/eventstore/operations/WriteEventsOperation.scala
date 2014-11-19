e4916fe8f481cfc6975d6f46916eb2d2057d8186
Yaroslav Klymko <t3hnar@gmail.com>
2014-11-19 21:40:23 +0200

    implement WriteEventsOperation

--- src/main/scala/eventstore/operations/WriteEventsOperation.scala
+++ src/main/scala/eventstore/operations/WriteEventsOperation.scala
@@ -7,7 +7,7 @@ import eventstore.tcp.PackOut
 
 import scala.util.{ Success, Failure, Try }
 
-case class ReadEventOperation(pack: PackOut, client: ActorRef, inFunc: InFunc, outFunc: Option[OutFunc]) extends Operation {
+case class WriteEventsOperation(pack: PackOut, client: ActorRef, inFunc: InFunc, outFunc: Option[OutFunc]) extends Operation {
   def id = pack.correlationId
 
   def inspectIn(in: Try[In]) = {
@@ -28,13 +28,26 @@ case class ReadEventOperation(pack: PackOut, client: ActorRef, inFunc: InFunc, o
     }
 
     in match {
-      case Success(_: ReadEventCompleted)                  => succeed()
+      case Success(_: WriteEventsCompleted)                            => succeed()
 
-      case Failure(EsException(EsError.EventNotFound, _))  => succeed()
+      case Failure(EsException(EsError.PrepareTimeout, _))             => retry()
 
-      case Failure(EsException(EsError.StreamNotFound, _)) => succeed()
+      case Failure(EsException(EsError.ForwardTimeout, _))             => retry()
 
-      case Failure(EsException(EsError.StreamDeleted, _))  => succeed()
+      case Failure(EsException(EsError.CommitTimeout, _))              => retry()
+
+      case Failure(EsException(EsError.WrongExpectedVersion, Some(_))) => succeed()
+
+      case Failure(x @ EsException(EsError.WrongExpectedVersion, None)) =>
+        val writeEvents = pack.message.asInstanceOf[WriteEvents]
+        val message = s"Write failed due to WrongExpectedVersion: ${writeEvents.streamId}, ${writeEvents.expectedVersion}"
+        val exception = x.copy(message = Some(message))
+        inFunc(Failure(exception))
+        None
+
+      case Failure(EsException(EsError.StreamDeleted, _))      => succeed()
+
+      case Failure(EsException(EsError.InvalidTransaction, _)) => succeed()
 
       case Failure(x @ EsException(EsError.NotHandled(reason), _)) => reason match {
         case NotReady     => retry()
@@ -45,8 +58,8 @@ case class ReadEventOperation(pack: PackOut, client: ActorRef, inFunc: InFunc, o
       case Failure(EsException(EsError.AccessDenied, Some(_))) => succeed()
 
       case Failure(x @ EsException(EsError.AccessDenied, None)) =>
-        val readEvent = pack.message.asInstanceOf[ReadEvent]
-        val exception = x.copy(message = Some(s"Read access denied for ${readEvent.streamId}"))
+        val writeEvents = pack.message.asInstanceOf[WriteEvents]
+        val exception = x.copy(message = Some(s"Write access denied for ${writeEvents.streamId}"))
         inFunc(Failure(exception))
         None
 
