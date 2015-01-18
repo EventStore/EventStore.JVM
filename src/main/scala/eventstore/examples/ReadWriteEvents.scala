56449dd7c373a8b6ecc79a7b8200abcf7f37a25d
Yaroslav Klymko <t3hnar@gmail.com>
2015-01-18 19:42:52 +0200

    implement ReadWriteEvents app, to be used to testing against cluster

---
+++ src/main/scala/eventstore/examples/ReadWriteEvents.scala
@@ -0,0 +1,76 @@
+package eventstore
+package examples
+
+import akka.actor.Status.Failure
+import akka.actor.{ActorLogging, Props, Actor, ActorSystem}
+import scala.util.Random
+import scala.concurrent.duration._
+
+object ReadWriteEvents extends App {
+  val system = ActorSystem()
+  system.actorOf(Props[ReadWriteEventsActor], "read-write")
+}
+
+class ReadWriteEventsActor extends Actor with ActorLogging {
+  import context.dispatcher
+
+  val maxCount = 10
+
+  val connection = EventStoreExtension(context.system).actor
+  val streamId = EventStream.Id(randomUuid.toString)
+
+  val rcvStreamNotFound: Receive = {
+    case Failure(_: StreamNotFoundException) =>
+      val events = write(ExpectedVersion.NoStream)
+      context become rcvWriteCompleted(events)
+    case x                                   =>
+      log.error(s"Received unexpected $x")
+      shutdown()
+  }
+
+  override def preStart() = read(EventNumber.First)
+
+  def receive = rcvStreamNotFound
+
+  def rcvWriteCompleted(events: List[EventData]): Receive = {
+    case WriteEventsCompleted(Some(range), _) =>
+      read(range.start)
+      context become rcvReadCompleted(events)
+    case x                                    =>
+      log.error(s"Received unexpected $x")
+      shutdown()
+  }
+
+  def rcvReadCompleted(events: List[EventData]): Receive = {
+    case x: ReadStreamEventsCompleted =>
+      if (x.events.map(_.data) != events) {
+        log.error("write.events must equal read.events {}", x)
+        shutdown()
+      } else if (!x.endOfStream) {
+        log.error("endOfStream must be true {}", x)
+        shutdown()
+      } else {
+        val events = write(ExpectedVersion.Exact(x.lastEventNumber))
+        context become rcvWriteCompleted(events)
+      }
+    case x                            =>
+      log.error(s"Received unexpected {}", x)
+      shutdown()
+  }
+
+  def write(expected: ExpectedVersion) = {
+    val bytes = new Array[Byte](100)
+    Random.nextBytes(bytes)
+    val eventType = randomUuid.toString
+    val events = List.fill(maxCount)(EventData(eventType, data = Content(bytes)))
+    connection ! WriteEvents(streamId, events, expected)
+    events
+  }
+
+  def read(number: EventNumber) = {
+    val msg = ReadStreamEvents(streamId, number, maxCount)
+    context.system.scheduler.scheduleOnce(100.millis, connection, msg)
+  }
+
+  def shutdown() = context.system.shutdown()
+}
