95faaf5d2c9a6789ac4f668077959aec3c60f67d
Yaroslav Klymko <t3hnar@gmail.com>
2015-01-18 22:44:47 +0200

    implement ClusterSettingsBuilder

--- src/main/scala/eventstore/j/ClusterSettingsBuilder.scala
+++ src/main/scala/eventstore/j/ClusterSettingsBuilder.scala
@@ -2,43 +2,59 @@ package eventstore
 package j
 
 import java.net.InetSocketAddress
-import eventstore.cluster.{ GossipSeedsOrDns, ClusterSettings }
-
+import cluster.ClusterSettings
+import cluster.GossipSeedsOrDns
+import cluster.GossipSeedsOrDns.{ GossipSeeds, ClusterDns }
 import scala.concurrent.duration._
 
 class ClusterSettingsBuilder extends Builder[ClusterSettings] with ChainSet[ClusterSettingsBuilder] {
-  protected var _gossipSeedsOrDns: GossipSeedsOrDns = ???
-  protected var _maxDiscoverAttempts = 10
-  protected var _gossipTimeout = 1.second
+  private val default = ClusterSettings()
+  protected var _gossipSeedsOrDns: GossipSeedsOrDns = default.gossipSeedsOrDns
+  protected var _dnsLookupTimeout = default.dnsLookupTimeout
+  protected var _maxDiscoverAttempts = default.maxDiscoverAttempts
+  protected var _discoverAttemptInterval = default.discoverAttemptInterval
+  protected var _discoveryInterval = default.discoveryInterval
+  protected var _gossipTimeout = default.gossipTimeout
 
-  def clusterDns(x: String): ClusterSettingsBuilder = set {
-    require(x != null, "clusterDns must be not null")
-    require(x.nonEmpty, "clusterDns must be not empty")
-    //    _clusterDns = x
+  def clusterDns(dns: String): ClusterSettingsBuilder = set { _gossipSeedsOrDns = ClusterDns(dns) }
+
+  def clusterDns(dns: String, port: Int): ClusterSettingsBuilder = set { _gossipSeedsOrDns = ClusterDns(dns, port) }
+
+  def gossipSeeds(seeds: InetSocketAddress*): ClusterSettingsBuilder = set {
+    _gossipSeedsOrDns = GossipSeeds(seeds.toList)
   }
 
-  def maxDiscoverAttempts(x: Int): ClusterSettingsBuilder = set {
-    _maxDiscoverAttempts = x
-  }
+  def dnsLookupTimeout(x: FiniteDuration): ClusterSettingsBuilder = set { _dnsLookupTimeout = x }
 
-  //  def externalGossipPort(x: Int): ClusterSettingsBuilder = set {
-  //    _externalGossipPort = x
-  //  }
+  def dnsLookupTimeout(length: Long, unit: TimeUnit): ClusterSettingsBuilder = dnsLookupTimeout(FiniteDuration(length, unit))
 
-  //  def gossipSeeds(x: InetSocketAddress*): ClusterSettingsBuilder = set {
-  //    _gossipSeeds = x.toList
-  //  }
+  def dnsLookupTimeout(seconds: Long): ClusterSettingsBuilder = dnsLookupTimeout(seconds, SECONDS)
 
-  def gossipTimeout(x: FiniteDuration): ClusterSettingsBuilder = set {
-    _gossipTimeout = x
-  }
+  def maxDiscoverAttempts(x: Int): ClusterSettingsBuilder = set { _maxDiscoverAttempts = x }
+
+  def discoverAttemptInterval(x: FiniteDuration): ClusterSettingsBuilder = set { _discoverAttemptInterval = x }
+
+  def discoverAttemptInterval(length: Long, unit: TimeUnit): ClusterSettingsBuilder = discoverAttemptInterval(FiniteDuration(length, unit))
+
+  def discoverAttemptInterval(seconds: Long): ClusterSettingsBuilder = discoverAttemptInterval(seconds, SECONDS)
+
+  def discoveryInterval(x: FiniteDuration): ClusterSettingsBuilder = set { _discoveryInterval = x }
+
+  def discoveryInterval(length: Long, unit: TimeUnit): ClusterSettingsBuilder = discoveryInterval(FiniteDuration(length, unit))
+
+  def discoveryInterval(seconds: Long): ClusterSettingsBuilder = discoveryInterval(seconds, SECONDS)
+
+  def gossipTimeout(x: FiniteDuration): ClusterSettingsBuilder = set { _gossipTimeout = x }
 
   def gossipTimeout(length: Long, unit: TimeUnit): ClusterSettingsBuilder = gossipTimeout(FiniteDuration(length, unit))
 
-  def gossipTimeout(length: Long): ClusterSettingsBuilder = gossipTimeout(length, SECONDS)
+  def gossipTimeout(seconds: Long): ClusterSettingsBuilder = gossipTimeout(seconds, SECONDS)
 
-  def build = ClusterSettings(
+  def build: ClusterSettings = ClusterSettings(
     gossipSeedsOrDns = _gossipSeedsOrDns,
+    dnsLookupTimeout = _dnsLookupTimeout,
     maxDiscoverAttempts = _maxDiscoverAttempts,
+    discoverAttemptInterval = _discoverAttemptInterval,
+    discoveryInterval = _discoveryInterval,
     gossipTimeout = _gossipTimeout)
 }
