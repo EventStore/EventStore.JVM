e205dfe592848e0be2d814fa4dbe2fa6a57432c3
Yaroslav Klymko <t3hnar@gmail.com>
2015-01-21 22:09:54 +0200

    add cluster to SettingsBuilder

--- src/main/scala/eventstore/j/SettingsBuilder.scala
+++ src/main/scala/eventstore/j/SettingsBuilder.scala
@@ -4,6 +4,7 @@ package j
 import Settings.Default
 import Builder.{ ResolveLinkTosSnippet, RequireMasterSnippet }
 import java.net.InetSocketAddress
+import eventstore.cluster.ClusterSettings
 import scala.concurrent.duration._
 
 class SettingsBuilder extends Builder[Settings]
@@ -20,46 +21,35 @@ class SettingsBuilder extends Builder[Settings]
   protected var _operationMaxRetries = Default.operationMaxRetries
   protected var _connectionTimeout = Default.connectionTimeout
   protected var _operationTimeout = Default.operationTimeout
-  protected var _readBatchSize: Int = Default.readBatchSize
+  protected var _readBatchSize = Default.readBatchSize
   protected var _backpressure = Default.backpressure
+  protected var _cluster = Default.cluster
 
-  def address(x: InetSocketAddress): SettingsBuilder = set {
-    _address = x
-  }
+  def address(x: InetSocketAddress): SettingsBuilder = set { _address = x }
 
   def address(host: String): SettingsBuilder = address(host :: Default.address.getPort)
 
-  def connectionTimeout(x: FiniteDuration): SettingsBuilder = set {
-    _connectionTimeout = x
-  }
+  def connectionTimeout(x: FiniteDuration): SettingsBuilder = set { _connectionTimeout = x }
 
   def connectionTimeout(length: Long, unit: TimeUnit): SettingsBuilder = connectionTimeout(FiniteDuration(length, unit))
 
   def connectionTimeout(seconds: Long): SettingsBuilder = connectionTimeout(seconds, SECONDS)
 
-  def maxReconnections(x: Int): SettingsBuilder = set {
-    _maxReconnections = x
-  }
+  def maxReconnections(x: Int): SettingsBuilder = set { _maxReconnections = x }
 
-  def reconnectionDelayMin(x: FiniteDuration): SettingsBuilder = set {
-    _reconnectionDelayMin = x
-  }
+  def reconnectionDelayMin(x: FiniteDuration): SettingsBuilder = set { _reconnectionDelayMin = x }
 
   def reconnectionDelayMin(length: Long, unit: TimeUnit): SettingsBuilder = reconnectionDelayMin(FiniteDuration(length, unit))
 
   def reconnectionDelayMin(seconds: Long): SettingsBuilder = reconnectionDelayMin(seconds, SECONDS)
 
-  def reconnectionDelayMax(x: FiniteDuration): SettingsBuilder = set {
-    _reconnectionDelayMax = x
-  }
+  def reconnectionDelayMax(x: FiniteDuration): SettingsBuilder = set { _reconnectionDelayMax = x }
 
   def reconnectionDelayMax(length: Long, unit: TimeUnit): SettingsBuilder = reconnectionDelayMax(FiniteDuration(length, unit))
 
   def reconnectionDelayMax(seconds: Long): SettingsBuilder = reconnectionDelayMax(seconds, SECONDS)
 
-  def defaultCredentials(x: Option[UserCredentials]): SettingsBuilder = set {
-    _defaultCredentials = x
-  }
+  def defaultCredentials(x: Option[UserCredentials]): SettingsBuilder = set { _defaultCredentials = x }
 
   def defaultCredentials(x: UserCredentials): SettingsBuilder = defaultCredentials(Option(x))
 
@@ -68,25 +58,19 @@ class SettingsBuilder extends Builder[Settings]
 
   def noDefaultCredentials: SettingsBuilder = defaultCredentials(None)
 
-  def heartbeatInterval(x: FiniteDuration): SettingsBuilder = set {
-    _heartbeatInterval = x
-  }
+  def heartbeatInterval(x: FiniteDuration): SettingsBuilder = set { _heartbeatInterval = x }
 
   def heartbeatInterval(length: Long, unit: TimeUnit): SettingsBuilder = heartbeatInterval(FiniteDuration(length, unit))
 
   def heartbeatInterval(seconds: Long): SettingsBuilder = heartbeatInterval(seconds, SECONDS)
 
-  def heartbeatTimeout(x: FiniteDuration): SettingsBuilder = set {
-    _heartbeatTimeout = x
-  }
+  def heartbeatTimeout(x: FiniteDuration): SettingsBuilder = set { _heartbeatTimeout = x }
 
   def heartbeatTimeout(length: Long, unit: TimeUnit): SettingsBuilder = heartbeatTimeout(FiniteDuration(length, unit))
 
   def heartbeatTimeout(seconds: Long): SettingsBuilder = heartbeatTimeout(seconds, SECONDS)
 
-  def operationTimeout(x: FiniteDuration): SettingsBuilder = set {
-    _operationTimeout = x
-  }
+  def operationTimeout(x: FiniteDuration): SettingsBuilder = set { _operationTimeout = x }
 
   def limitAttemptsForOperationTo(limit: Int): SettingsBuilder = {
     require(limit >= 1, s"limit must be >= 1 but is $limit")
@@ -102,9 +86,7 @@ class SettingsBuilder extends Builder[Settings]
     operationMaxRetries(-1)
   }
 
-  def operationMaxRetries(x: Int): SettingsBuilder = set {
-    _operationMaxRetries = x
-  }
+  def operationMaxRetries(x: Int): SettingsBuilder = set { _operationMaxRetries = x }
 
   def operationTimeout(length: Long, unit: TimeUnit): SettingsBuilder = operationTimeout(FiniteDuration(length, unit))
 
@@ -116,13 +98,11 @@ class SettingsBuilder extends Builder[Settings]
   override def performOnMasterOnly: SettingsBuilder = super.performOnMasterOnly
   override def requireMaster(x: Boolean): SettingsBuilder = super.requireMaster(x)
 
-  def readBatchSize(x: Int): SettingsBuilder = set {
-    _readBatchSize = x
-  }
+  def readBatchSize(x: Int): SettingsBuilder = set { _readBatchSize = x }
 
-  def backpressure(x: BackpressureSettings): SettingsBuilder = set {
-    _backpressure = x
-  }
+  def backpressure(x: BackpressureSettings): SettingsBuilder = set { _backpressure = x }
+
+  def cluster(x: ClusterSettings): SettingsBuilder = set { _cluster = Some(x) }
 
   def build: Settings = Settings(
     address = _address,
@@ -137,5 +117,6 @@ class SettingsBuilder extends Builder[Settings]
     resolveLinkTos = _resolveLinkTos,
     requireMaster = _requireMaster,
     readBatchSize = _readBatchSize,
-    backpressure = _backpressure)
+    backpressure = _backpressure,
+    cluster = _cluster)
 }
