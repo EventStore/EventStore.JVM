fcca0c93d1970606b57cbee102d78d9d47dc4526
Yaroslav Klymko <t3hnar@gmail.com>
2015-01-28 21:17:25 +0200

    fix EventStoreExtensionSpec

--- src/test/scala/eventstore/EventStoreExtensionSpec.scala
+++ src/test/scala/eventstore/EventStoreExtensionSpec.scala
@@ -3,22 +3,19 @@ package eventstore
 import akka.actor.Status.Failure
 import scala.concurrent.Await
 import scala.concurrent.duration._
-import scala.util.Properties
 
 class EventStoreExtensionSpec extends util.ActorSpec {
-  Properties.setProp("eventstore.address.port", "0")
-
   val readEvent = ReadEvent(EventStream.Id(randomUuid.toString))
 
   "EventStoreExtension" should {
     "return connection actor" in new ActorScope {
       EventStoreExtension(system).actor ! readEvent
-      expectMsgPF() { case Failure(_: CannotEstablishConnectionException) => }
+      expectMsgPF() { case Failure(_: EsException) => }
     }
 
     "return connection instance" in new ActorScope {
       val future = EventStoreExtension(system).connection.future(readEvent)
-      Await.result(future, 3.seconds) must throwAn[CannotEstablishConnectionException]
+      Await.result(future, 3.seconds) must throwAn[EsException]
     }
   }
 }
