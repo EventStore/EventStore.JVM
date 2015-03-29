6c49baf63019645c8ca8ad23f6e5a964c5265938
Yaroslav Klymko <t3hnar@gmail.com>
2015-03-29 16:48:46 +0300

    add license clause generated pom

--- project/Publish.scala
+++ project/Publish.scala
@@ -9,6 +9,12 @@ object Publish {
       <connection>scm:git:git@github.com:EventStore/EventStore.JVM.git</connection>
       <developerConnection>scm:git:git@github.com:EventStore/EventStore.JVM.git</developerConnection>
     </scm>
+    <licenses>
+      <license>
+        <name>BSD 3-Clause</name>
+        <url>http://raw.github.com/EventStore/EventStore.JVM/master/LICENSE</url>
+      </license>
+    </licenses>
     <developers>
       <developer>
         <id>t3hnar</id>
